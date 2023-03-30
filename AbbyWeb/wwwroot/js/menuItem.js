049c274c732da9ce2fe8b3dce996593c1f5bef5b
Connor McLean <connorm11111@gmail.com>
2023-03-31 01:09:37 +0200

    Edit and Delete UI buttons

--- AbbyWeb/wwwroot/js/menuItem.js
+++ AbbyWeb/wwwroot/js/menuItem.js
@@ -10,7 +10,23 @@ $(document).ready(function () {
         },
         "columns": [
             { "data": "name", "width": "25%" },
-            { "data": "price", "width": "25%" }
+            { "data": "price", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "foodType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div>
+                                <a href="/Admin/MenuItems/Upsert?id=${data}" class="btn btn-success text-white"
+                                style="cursor:pointer; width:100px;"> <i class="bi bi-pencil-square"></i> Edit </a>
+                                <a href="/Admin/MenuItems/Upsert?id=${data}" class="btn btn-danger text-white"
+                                style="cursor:pointer; width:100px;"> <i class="bi bi-trash-fill"></i> Delete </a>
+
+                            </div`
+                },
+
+                "width": "15%"
+            }
 
         ],
         "width": "100%"
