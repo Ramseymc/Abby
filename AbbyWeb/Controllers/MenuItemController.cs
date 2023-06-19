f2fd4c1252e9333dcb000c2d7f875a47603c29d0
Connor McLean <connorm11111@gmail.com>
2023-06-19 16:45:03 +0200

    HTTTP Delete For Menu Item

--- AbbyWeb/Controllers/MenuItemController.cs
+++ AbbyWeb/Controllers/MenuItemController.cs
@@ -8,8 +8,10 @@ namespace AbbyWeb.Controllers
     public class MenuItemController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public MenuItemController(IUnitOfWork unitOfWork)
+
+        public MenuItemController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
         }
@@ -19,6 +21,17 @@ namespace AbbyWeb.Controllers
         {
             var menuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
             return Json(new {data = menuItemList});
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id = id);
+
+            _unitOfWork.MenuItem.Remove(objFromDb);
+            _unitOfWork.Save();
+            var menuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
+            return Json(new { data = menuItemList });
         }
     }
 }
